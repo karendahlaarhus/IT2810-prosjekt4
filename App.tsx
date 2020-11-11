2979f7cfb435d839246993d662f517aef328536d
Johanna Sofie Slinde <JohannaSofie@Johannas-MacBook-Air-2.local>
2020-11-11 14:04:54 +0100

    adds padding for perfect mobile view #1

--- App.tsx
+++ App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Provider } from 'react-redux';
+import React from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import { Provider } from "react-redux";
 import store from "./store/store";
-import  RecipeContainer from './components/RecipeContainer';
+import RecipeContainer from "./components/RecipeContainer";
 
 export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
         <RecipeContainer />
-     </View>
-     </Provider>
+      </View>
+    </Provider>
   );
 }
 
+var { height, width } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    alignItems: "stretch",
+    justifyContent: "center",
+    paddingTop: height * 0.1,
+    paddingLeft: width * 0.1,
+    paddingRight: width * 0.1,
   },
 });
